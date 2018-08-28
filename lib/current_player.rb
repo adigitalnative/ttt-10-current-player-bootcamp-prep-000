a9a839aa4db2147236b4fa84e4bef362db1bd24d
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 04:21:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,5 +9,16 @@ def turn_count(board)
 end
 
 def current_player(board)
+  x_counter = 0
+  board.each do |slot|
+    if slot == "X"
+      x_counter += 1
+    end
+  end
   
+  if x_counter % 2 == 0
+    true
+  else
+    false
+  end
 end
