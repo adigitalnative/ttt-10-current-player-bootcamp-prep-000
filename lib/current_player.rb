2900b2992ce7be74beb29d4a6f6d66ff54cc7e66
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 04:27:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,15 +10,19 @@ end
 
 def current_player(board)
   x_count = 0
+  y_count = 0
   board.each do |slot|
     if slot == "X"
       x_count += 1
+    elsif slot == "Y"
+      y_count += 1
     end
   end
   
   if x_count % 2 == 0
     "X"
   else
+    raise x_count.inspect
     "O"
   end
 end
